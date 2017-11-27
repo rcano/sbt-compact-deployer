b6b7ede38b414b1a12c1fd1e20b8d4b8393155a1
rcano <ioniviil@gmail.com>
2017-11-27 00:14:23 -0300

    added more debugging control

--- launcher/src/main/java/Launcher.java
+++ launcher/src/main/java/Launcher.java
@@ -10,12 +10,16 @@ public class Launcher {
   public static void main(String[] args) throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
     PackedAppClassLoader packedApp = new PackedAppClassLoader(Launcher.class.getResourceAsStream("/app"));
 
-    String chosenMainClass = System.getProperty("launcher-main-class");
+    if (Boolean.getBoolean("launcherDumpIndex")) {
+      packedApp.index.forEach((key, value) -> System.out.println(key));
+    }
+    
+    String chosenMainClass = System.getProperty("launcherMainClass");
     String mainClass = null;
     if (chosenMainClass != null) {
       mainClass = chosenMainClass;
     } else {
-      Manifest manifest = new Manifest(new ByteArrayInputStream(packedApp.getEntry("META-INF/MANIFEST.MF")));
+      Manifest manifest = new Manifest(new ByteArrayInputStream(packedApp.index.get("META-INF/MANIFEST.MF")));
       Attributes values = manifest.getMainAttributes();
       mainClass = values.getValue("Main-Class");
       if (mainClass == null) throw new IllegalStateException("No main class found in app jar");
